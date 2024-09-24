45c879469f63a282fbc7334914569220bdeca0d4
Danny79k <belloli.danny@gmail.com>
2024-09-24 23:32:15 +0100

    aguas peligrosas, intentando quitar el marco co nel boton X

--- ProyectoCarrito/js/app.js
+++ ProyectoCarrito/js/app.js
@@ -1,10 +1,10 @@
 // Variables, debes hacer el querySelector adecuado
 
-const carrito = document.querySelector("#carrito"); //Busca el primer elemento cuyo id sea "carrito"
-const listaCursos = document.querySelector("#lista-cursos"); //Busca el primer elemento cuyo id sea "lista-cursos"
-const contenedorCarrito = document.querySelector("#lista-carrito tbody"); //Busca el primer elemento tbody dentro del elemento con id lista-carrito
-const vaciarCarritoBtn = document.querySelector("#vaciar-carrito"); //Busca el primer elemento cuyo id sea vaciar-carrito
-const tarjetasCursos= document.querySelectorAll(".card"); //Busca todos los elementos cuya clase sea curso
+const carrito = document.getElementById("carrito")//Busca el primer elemento cuyo id sea "carrito"
+const listaCursos = document.getElementById("lista-cursos") //Busca el primer elemento cuyo id sea "lista-cursos"
+const contenedorCarrito = document.getElementsByTagName("tbody")[0] //Busca el primer elemento tbody dentro del elemento con id lista-carrito
+const vaciarCarritoBtn = document.getElementById("vaciar-carrito") //Busca el primer elemento cuyo id sea vaciar-carrito
+const tarjetasCursos = document.getElementsByClassName("card"); //Busca todos los elementos cuya clase sea curso
 let articulosCarrito = [];
 
 // Listeners
@@ -19,23 +19,26 @@ function cargarEventListeners() {
 
      // Al Vaciar el carrito
      vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+     vaciarCarritoBtn.addEventListener('click', despintarTodosBorde);
 
 
      // NUEVO: Contenido cargado
      document.addEventListener('DOMContentLoaded', () => {
-          articulosCarrito = JSON.parse( localStorage.getItem('carrito') ) || []  ;
+          articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
           // console.log(articulosCarrito);
           carritoHTML();
      });
 }
 
 
-// Función que añade el curso al carrito
+//#region // Función que añade el curso al carrito
 function agregarCurso(e) {
      e.preventDefault();
      // Delegation para agregar-carrito
-     if(e.target.classList.contains('agregar-carrito')) {
+     if (e.target.classList.contains('agregar-carrito')) {
           const curso = e.target.parentElement.parentElement;
+          pintaBorde(curso)
+          // pintarBordeVerdeMismoAutor(curso)
           // Enviamos el curso seleccionado para tomar sus datos
           leerDatosCurso(curso);
      }
@@ -48,48 +51,53 @@ function leerDatosCurso(curso) {
           imagen: curso.querySelector("img").src, //La imagen del curso
           titulo: curso.querySelector(".info-card h4").textContent, //el título del curso
           precio: curso.querySelector(".precio").textContent, //el precio con el descuento ya aplicado
-          id: curso.querySelector("a[data-id]"), //Vamos a buscar el data-id del curso, primero buca el elemento y luego accede al atributo
+          id: curso.querySelector("a").getAttribute("data-id"), //Vamos a buscar el data-id del curso, primero buca el elemento y luego accede al atributo
+          propietario: curso.querySelector(".info-card p"), //Vamos a buscar el data-id del curso, primero buca el elemento y luego accede al atributo
           cantidad: 1
      }
      console.log(infoCurso.id);
+     console.log(infoCurso.propietario);
 
-     if( articulosCarrito.some( curso => curso.id === infoCurso.id ) ) { 
-          const cursos = articulosCarrito.map( curso => {
-               if( curso.id === infoCurso.id ) {
+     if (articulosCarrito.some(curso => curso.id === infoCurso.id)) {
+          const cursos = articulosCarrito.map(curso => {
+               if (curso.id === infoCurso.id) {
                     let cantidad = parseInt(curso.cantidad);
                     cantidad++
-                    curso.cantidad =  cantidad;
+                    curso.cantidad = cantidad;
                     return curso;
                } else {
                     return curso;
                }
           })
           articulosCarrito = [...cursos];
-     }  else {
+     } else {
           articulosCarrito = [...articulosCarrito, infoCurso];
      }
 
      console.log(articulosCarrito)
 
-     
+
 
      // console.log(articulosCarrito)
      carritoHTML();
 }
 
-// Elimina el curso del carrito en el DOM
+//#region Elimina el curso del carrito en el DOM
 function eliminarCurso(e) {
      e.preventDefault();
-     if(e.target.classList.contains('borrar-curso') ) {
-          e.target.parentElement.parentElement.remove();
+     console.log(articulosCarrito);
+     if (e.target.classList.contains('borrar-curso')) {
+          // e.target.parentElement.parentElement.remove();
           const curso = e.target.parentElement.parentElement;
           const cursoId = curso.querySelector('a').getAttribute('data-id');
           
+          despintarBordeSeleccionado(curso, cursoId)
+
           // Eliminar del arreglo del carrito
           articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
-
           carritoHTML();
      }
+     console.log(articulosCarrito);
 }
 
 
@@ -128,7 +136,43 @@ function sincronizarStorage() {
 // Elimina los cursos del carrito en el DOM
 function vaciarCarrito() {
      // forma rapida (recomendada)
-     while(contenedorCarrito.firstChild) {
+     while (contenedorCarrito.firstChild) {
           contenedorCarrito.removeChild(contenedorCarrito.firstChild)
-      }
+          localStorage.clear();
+     }
 }
+
+function despintarBordeSeleccionado(curso, cursoId){
+
+     console.log(cursoId);
+     console.log(curso => curso.id);
+}
+
+function despintarTodosBorde(){
+
+     Array.prototype.forEach.call(tarjetasCursos, function (_tarjetaCurso) {
+               _tarjetaCurso.style.borderStyle = ""
+               _tarjetaCurso.style.borderWidth = ""
+               _tarjetaCurso.style.borderColor = ""
+     });
+}
+
+function pintaBorde(curso) {
+
+     curso.style.borderStyle = "solid"
+     curso.style.borderWidth = "5px"
+     curso.style.borderColor = "blue"
+     curso.setAttribute("tipo", "seleccionado")
+}
+
+// function pintarBordeVerdeMismoAutor(curso) {
+//      leerDatosCurso(curso)
+//      let cursoPropietario = curso.querySelector(".info-card p")
+//      Array.prototype.forEach.call(tarjetasCursos, function (_tarjetaCurso) {
+//           if (infoCurso.propietario === cursoPropietario) {
+//                curso.style.borderStyle = "solid"
+//                curso.style.borderWidth = "5px"
+//                curso.style.borderColor = "green"
+//           }
+//      });
+// }
